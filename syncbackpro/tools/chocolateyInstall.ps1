b5306a1c1a94dbd3eff87703b06e33dd4d75f215
rals01 <rals01@gmail.com>
2013-10-29 18:30:55 +0100

    added syncbackpro

---
+++ syncbackpro/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'syncbackpro.install' 
+$installerType = 'exe' 
+$url = 'http://www.2brightsparks.com/assets/software/SyncBackPro_Setup.exe' 
+$url64 = $url
+$silentArgs = ' /verysilent'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
