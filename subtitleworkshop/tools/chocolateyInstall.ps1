fcac652f27c00badc0c603bc853260fa28e24de5
rals01 <rals01@gmail.com>
2013-11-03 15:38:32 +0100

    Fixed package name for subtitleworkshop (copy/waste error)

--- subtitleworkshop/tools/chocolateyInstall.ps1
+++ subtitleworkshop/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-﻿$packageName = 'garminantagent' 
+﻿$packageName = 'subtitleworkshop' 
 $installerType = 'exe'
 $url = 'http://downloads.sourceforge.net/project/subworkshop/bin/Subtitle%20Workshop%206.0a/SubtitleWorkshop_6.0a_130825_installer.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsubworkshop%2F&ts=1383488124&use_mirror=heanet' 
 $url64 = $url
